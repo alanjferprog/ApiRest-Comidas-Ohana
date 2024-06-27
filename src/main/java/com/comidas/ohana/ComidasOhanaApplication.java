0aafe43b00f55a9fcc955c3b627e72d76b048508
Alan <alanfernandez97@hotmail.com>
2024-06-26 22:41:03 -0300

    primer commit

---
+++ src/main/java/com/comidas/ohana/ComidasOhanaApplication.java
@@ -0,0 +1,13 @@
+package com.comidas.ohana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComidasOhanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComidasOhanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comidas/ohana/ComidasOhanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comidas.ohana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidasOhanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
